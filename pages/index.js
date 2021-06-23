4d9d3190f5bea9d09998d234ea0ca0f7e43eee68
William Wong <wmwwong@legco.gov.hk>
2021-06-23 15:14:48 +0800

    global css setup

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
             <a>this page</a>
           </Link>
         </h1>
+   
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>

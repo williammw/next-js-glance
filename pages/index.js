a0d4b84ec24c55690707e43d9d068801477e12be
William Wong <wmwwong@legco.gov.hk>
2021-06-29 17:29:02 +0800

    session

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,34 @@
 import Head from 'next/head'
 import Results from '../components/Results'
-// import utilStyles from '../styles/utils.module.css'
-// import { getSortedPostsData } from '../lib/posts'
 import requests from '../utils/requests'
-
-import Layout from '../components/Layout'
 import { getSession } from 'next-auth/client'
 import { signIn, signOut, useSession } from 'next-auth/client'
+import StorageOutlinedIcon from '@material-ui/icons/StorageOutlined';
 
 
 
-
- function Home({results}) {
+function Home({results}) {
   const [ session, loading ] = useSession()
   if (typeof window !== 'undefined' && loading) return null
   // if (!session) { return  (<><AccessDenied/></>)}
   return (
-    <Layout home> 
-    {!session && <>
-      Not signed in <br/>
-      <button onClick={() => signIn()}>Sign in</button>
-    </>}
-    {session && <>
-      
-      Signed in as {session.user.email} <br/>
+    <>   
+        <Head>
+            <title>Winning Everyday</title>
+        </Head>
+    
+      {!session && <>
+        Not signed in <br/>
+        <button onClick={() => signIn()}>Sign in</button>
+      </>}
+      {session && <>
+        Signed in as {session.user.email} <br/>
       <button onClick={() => signOut()}>Sign out</button>
-      <Head>
-          <title>title</title>
-      </Head>
-        <Results results={results}/>
-      
-    </>}
-    </Layout>
- 
-
+        
+      <Results results={results}/>
+      </>
+      }
+    </>
   )
 }
 //https://api.themoviedb.org/3/trending/all/day?api_key=4296c14b497b17acadd5928634804c12

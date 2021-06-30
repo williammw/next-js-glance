01a84e9c8a518db017b170442daf5ed97560aa74
wwwwwmw <william.manwai@gmail.com>
2021-07-01 01:52:30 +0800

    fix ssr ccs issue(temp)

--- pages/index.js
+++ pages/index.js
@@ -4,34 +4,41 @@ import Results from '../components/Results'
 // import { getSortedPostsData } from '../lib/posts'
 import requests from '../utils/requests'
 import { getSession } from 'next-auth/client'
-import Layout from '../components/Layout'
+// import Layout from '../components/Layout'
 import { signIn, signOut, useSession } from 'next-auth/client'
-
-
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ProTip from '../src/ProTip';
+import Link from '../src/Link';
+import Copyright from '../src/Copyright';
+import dynamic from 'next/dynamic'
+import TopNav from '../components/TopNav';
 
 
  function Home({results}) {
   const [ session, loading ] = useSession()
-  // if (typeof window !== 'undefined' && loading) return null
-  // if (!session) { return  (<><AccessDenied/></>)}
-  return (
-    <> 
-    {!session && <>
-    {/* Sign In Page or public content */}
-      Not signed in <br/>
-      
-      <button onClick={() => signIn()}>Sign in</button>
-    </>}
-    {session && <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-      <Head>
-          <title>title</title>
-      </Head>
-      <Results results={results}/>
-    </>}
-  </>
+  if (typeof window !== 'undefined' && loading) return null
 
+
+  return (
+    // 
+    <>
+    <TopNav/>
+    <Container maxWidth="sm">
+    <Box my={4}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Next.js example
+      </Typography>
+      <Link href="/about" color="secondary">
+        Go to the about page
+      </Link>
+      <ProTip />
+      <Copyright />
+    </Box>
+  </Container>
+  </>
   )
 }
 //https://api.themoviedb.org/3/trending/all/day?api_key=4296c14b497b17acadd5928634804c12
@@ -53,3 +60,18 @@ export async function getServerSideProps(context){
 //https://next-auth.js.org/getting-started/client
 
 export default Home
+
+
+    // {!session && <>
+    // {/* Sign In Page or public content */}
+    //   Not signed in <br/>
+      
+    //   <button onClick={() => signIn()}>Sign in</button>
+    // </>}
+    // {session && <>
+    //   Signed in as {session.user.email} <br/>
+    //   <button onClick={() => signOut()}>Sign out</button>
+    //   <Head>
+    //       <title>title</title>
+    //   </Head>
+    //   <Results results={results}/></>}

9b75fe676b838a703222ed2aa4f058d0a2bb491c
wwwwwmw <william.manwai@gmail.com>
2021-07-18 06:05:09 +0800

    get data from firebase

--- pages/reports/hsif.js
+++ pages/reports/hsif.js
@@ -2,11 +2,15 @@ import SortableTable from '../../components/sortable-table'
 import Head from 'next/head'
 import { getSession } from 'next-auth/client'
 import { signIn, signOut, useSession } from 'next-auth/client'
-import Layout from '../../components/Layout'
-function hsif() {
+import db from '../../utils/firebase/firestore'
+
+// https://blog.jarrodwatts.com/the-ultimate-guide-to-firebase-with-nextjs
+function hsif({posts}) {
   const [ session, loading ] = useSession()
 
   console.log('session',session)
+  console.log('posts',posts)
+  
   if (typeof window !== 'undefined' && loading) return null
 
   return (
@@ -26,5 +30,30 @@ function hsif() {
     </>
   )
 }
+
+export async function  getServerSideProps(context){
+  let posts = {}
+  
+    // await the promise
+    const querySnapshot = await db
+      .collection('hsif')
+      .get();
+      https://stackoverflow.com/questions/66064397/nextjs-how-to-correctly-use-getstaticprops-for-firebase
+    // "then" part after the await
+    querySnapshot.forEach(function (doc) {
+      // console.log(doc.data())
+      posts = JSON.parse(JSON.stringify(doc.data()))
+    })
+    // console.log(posts)
+  return {
+      props: {
+        posts
+      }
+  }
+}
+
+
+
+
 
 export default hsif
